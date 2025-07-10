613ee1023c1dce0385379871e2fa1c91026bae32
ISPL <tatsam.shrestha@info.com.np>
2025-07-10 18:24:17 +0545

    change

---
+++ Models/Guide.cs
@@ -0,0 +1,11 @@
+﻿namespace UserRoles.Models
+{
+    public class Guide
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+
+
+    }
+}
